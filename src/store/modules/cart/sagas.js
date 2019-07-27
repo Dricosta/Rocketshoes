7312b7dc0076a2f90ec1306de24ac7a6315a3549
Dricosta <adrianoc55@hotmail.com>
2019-07-27 13:43:56 -0300

    adicionando history nas rotas para dar redirect ao adicionar no carrinho

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,8 +2,9 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 // function* representa um async - await mais poderoso
 // yield = await
@@ -24,7 +25,7 @@ function* addToCart({ id }) {
     }
 
     if (productExists) {
-        yield put(updateAmount(id, amount));
+        yield put(updateAmountSuccess(id, amount));
     } else {
         const response = yield call(api.get, `products/${id}`);
 
@@ -35,7 +36,25 @@ function* addToCart({ id }) {
         };
 
         yield put(addToCartSuccess(data));
+        history.push('/cart');
     }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+    if (amount <= 0) return;
+
+    const stock = yield call(api.get, `stock/${id}`);
+    const stokeAmount = stock.data.amount;
+
+    if (amount > stokeAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+    }
+
+    yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+    takeLatest('@cart/ADD_REQUEST', addToCart),
+    takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount)
+]);
